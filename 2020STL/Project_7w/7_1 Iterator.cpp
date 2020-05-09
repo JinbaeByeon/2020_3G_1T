a20957fa046403a0905584c19170eada6431f7b8
JinbaeByeon <quswlsqo@gmail.com>
2020-05-09 19:16:00 +0900

    Add files via upload
    7주 1일차 랜덤반복자 구현

--- 2020STL/Project_7w/7_1 Iterator.cpp
+++ 2020STL/Project_7w/7_1 Iterator.cpp
@@ -1,38 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
-#include <forward_list>
-#include <list>
-#include <deque>
-#include <array>
-#include <vector>
-#include <iterator>
+#include <algorithm>
 #include "String.h"
+#include <string>
 using namespace std;
 
-template<class T>
-void showCategory(const T&);
+int main() {
+	String str{ "The quick brown fox jumps over the lazy dog" };
+	sort(str.begin(), str.end());
 
-int main()
-{
-	istream_iterator<char> in_iter;
-	ostream_iterator<short> out_iter{ cout };
-	forward_list<int>::iterator forward_list_iter;
-	list<float>::iterator list_iter;
-	deque<double>::iterator deque_iter;
-	array<String, 0>::iterator array_iter;
-	vector<vector<char>>::iterator vector_iter;
-
-	showCategory(in_iter);
-	showCategory(out_iter);
-	showCategory(forward_list_iter);
-	showCategory(list_iter);
-	showCategory(deque_iter);
-	showCategory(array_iter);
-	showCategory(vector_iter);
-
-}
-
-template<class T>
-void showCategory(const T&)
-{
-	cout << typeid(iterator_traits<T>::iterator_category).name() << endl;
+	cout << str << endl;
 }
